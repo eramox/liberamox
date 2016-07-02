665a4e462d1932cec0b1dc17e56ab66ed7fa7f2d
eramox <lenormandpostbac@gmail.com>
2016-07-02 02:35:11 +0200

    remove CTest from test suite (#15)

--- runTest.sh
+++ runTest.sh
@@ -1,20 +1,7 @@
 echo "Starting test"
 
-run_ctest=1
-run_gtest=1
-
-res_ctest=0
-res_gtest=0
-
 # moving to test folder
 cd liberamox/build/testing
-
-# Cmake Test
-if [ $run_ctest -eq 1 ]; then
-echo "Running CTest:"
-ctest
-res_ctest=$?
-fi
 
 # Google test
 if [ $run_gtest -eq 1 ]; then
@@ -26,20 +13,15 @@ fi
 cd -
 echo "Test Finished"
 
-exit $res_ctest + $res_gtest
+exit $res_gtest
 
 if [ "$1" == "" ]
 then
 	echo "no parameter, executing all";
-	run_ctest=1;
 	run_gtest=1;
 else
 	if [ "$1" == "--gtest" ]
 	then
 		run_gtest=1;
-	elif [ "$1" == "--ctest" ]
-	then
-		run_ctest=1;
-	fi
 fi
 
